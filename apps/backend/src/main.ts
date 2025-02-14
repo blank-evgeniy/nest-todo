92b32ec154655b1e77d79254b7801da67e2117b9
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-02-14 21:54:11 +0200

    back/feature/cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,8 +5,16 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
+
   await app.listen(port);
   console.log(`Сервер запущен на http://localhost:${port}`);
 }
