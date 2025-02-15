061791bfb7c3b93871faa2ad7b24ac1caec5e3a9
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-02-15 09:54:04 +0200

    feature/tanstack-query-setup

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import router from './router';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from './providers/query/queryClient';
 
 declare module '@tanstack/react-router' {
   interface Register {
@@ -13,8 +14,8 @@ const rootElement = document.getElementById('root')!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
-    <StrictMode>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-    </StrictMode>
+    </QueryClientProvider>
   );
 }
