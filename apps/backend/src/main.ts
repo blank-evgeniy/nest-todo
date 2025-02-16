53915f89078af466f1b96594c7de317b2c44ea12
Blank Evgeniy <blank.eugeny@yandex.ru>
2025-02-16 11:19:57 +0200

    deploy

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173', 'https://nesttodo-front.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
     credentials: true,
